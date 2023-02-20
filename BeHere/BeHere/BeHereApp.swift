cc80dfa716271f1751a92ad7bd42e209de3fe596
makdox <86933663+makdox@users.noreply.github.com>
2023-02-19 19:39:51 -0500

    Add files via upload

---
+++ BeHere/BeHere/BeHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeHereApp.swift
+//  BeHere
+//
+//  Created by Max Donets on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct BeHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeHere/BeHere/Views/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BeHere
+//
+//  Created by Max Donets on 16.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
